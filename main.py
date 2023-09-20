46819323062704a77068bb84fc154e8c1d3b3045
AbdElrhmansalah <37964553+AbdElrhmansalah@users.noreply.github.com>
2023-09-20 14:04:22 +0300

    Update and rename embedding.py to main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 def process(input_texts):
     embeddings = model.encode(input_texts, normalize_embeddings=True)
     return embeddings
-@app.post("/embedText")
+@app.post("/")
 async def text(text: dict):
     string = text["text"]
     embeddings = process(string)
